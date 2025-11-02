773b61b3281a35391374f10240332128406e6bac
cos-alpha07 <kaushalkumarpal07@gmail.com>
2025-11-02 13:49:14 +0530

    MAIN update

--- MAIN.py
+++ MAIN.py
@@ -9,13 +9,14 @@ Created on Tue Sep  2 22:40:53 2025
 from specimen_files.unibar_redXs import *
 
 #%% material props
-D = define_constitutive_stiffness_matrix(E, nu, 'PLANE_STRESS')
+D = define_constitutive_stiffness_matrix(E, nu, 'PLANE_STRAIN')
 
 #%% [INITIALIZING] nodal state variables
 Ug = zeros(2*len(node_data))
 dU = zeros(2*len(node_data))
-PHIg = zeros(len(node_data))
-dPHI = zeros(len(node_data))
+Phig = zeros(len(node_data))
+dPhi = zeros(len(node_data))
+hist_param = zeros(len(gpts_crd))
 
 u1_df, u2_df, phi_df = initialize_save_files(file_save_directory, probname)
 
@@ -35,6 +36,7 @@ TS_mat = empty([0, 3])
 #%%% NR begins
 while app_load < umax:    # loop on steps
     dU[:] = 0.0
+    dPhi[:] = 0.0
     app_load += u_step
     
     stp += 1
@@ -42,18 +44,23 @@ while app_load < umax:    # loop on steps
     t1 = time.process_time()
     
     tempu = Ug.copy()
-
+    tempp = Phig.copy()
+    
     err_ = 1
     itr = 0
+    
     # loop for iterations
     while err_ > tol:
         itr += 1
-        print(f'({stp} (load={app_load}))\t Iteration - {itr}', end=', ')
+        print(f'({stp}) load={app_load}\t Iteration - {itr}', end=', ')
         
         # build Kmat, (Fint, Fext) -> Resd
-        K_mat, F_int = assemble_KK_Fi(elem_data, node_data, tempu, D)
-        #K_disp, K_phi, F_disp, F_phi = assemble_KK_Fi(elem_data, node_data, tempu, tempd, D)
-                
+        K_mat, K_phi, F_int, F_phi = assemble_forces_and_stiffness(elem_data, node_data, tempu, tempp, D,
+                                                                   hist_param, E, f_t, G_c, l_c, thk)
+        #K_disp, K_phi, F_disp, F_phi = assemble_KK_Fi(elem_data, node_data, tempu, tempp, D)
+        
+        # [DISP]
+        
         # applying boundary conditions
         K_ff = K_mat[ix_(fdofs, fdofs)]
         K_fp = K_mat[ix_(fdofs, pdofs)]
@@ -72,12 +79,20 @@ while app_load < umax:    # loop on steps
         # adding dU to tempu += dU
         tempu[load_dofs] = 0
         tempu += dU
-        err_ = norm(dU)/(norm(tempu) + 1e-8)
+        
+        # [PHI]
+        dPhi = solve(K_phi, -F_phi)
+        tempp += dPhi
+        
+        err_disp = norm(dU)/(norm(tempu) + 1e-8)
+        err_phi = norm(dPhi)/(norm(tempp) + 1e-8)
+        err_ = max(err_disp, err_phi)
         
         # convergence check
         if check_convergence(err_, tol):
             step_time = time.process_time() - t1
             Ug = tempu.copy()
+            Phig = tempp.copy()
             
             # updating plot-data
             SR_mat = append(SR_mat, [[app_load, -sum(F_int[fix_dofs])]], axis=0)
@@ -86,13 +101,21 @@ while app_load < umax:    # loop on steps
             print(f'  **> Solution converged at iteration-{itr}')
             break
         else:
-            print(f'error={err_:5e}')
+            print(f'err_disp={err_disp:5e}, err_phi={err_phi:5e}, error={err_:5e}')
     
     # creating dataframes
     u1_df[f'Step-{stp}'] = Series(Ug[[2*i for i in range(node_count)]], index=range(node_count))
     u2_df[f'Step-{stp}'] = Series(Ug[[2*i + 1 for i in range(node_count)]], index=range(node_count))
-    phi_df[f'Step-{stp}'] = Series(PHIg)
-    save_my_results(u1_df, u2_df, phi_df, zeros(2), SR_mat, TS_mat, elem_data, node_data, zeros([2, 4]), save_loc=fr'{file_save_directory}\{probname}')
+    phi_df[f'Step-{stp}'] = Series(Phig)
+    save_my_results(u1_df, u2_df, phi_df, zeros(2), SR_mat, TS_mat, elem_data, node_data, zeros([2, 4]),
+                    save_loc=fr'{file_save_directory}\{probname}')
+
+sys.exit()
+
+#%% Intermittent Plotting
+plot_contour(plot_this=Ug[[2*i for i in range(node_count)]], plot_on='nodes', crds=node_data, text_TT='U1',
+             stpNum=stp, fill_data=fill_data)
+
     
     
     
