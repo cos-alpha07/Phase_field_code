a9496805d518bc5ce06d5bb15850e7385dd131ca
cos-alpha07 <kaushalkumarpal07@gmail.com>
2026-01-07 13:04:12 +0530

    update

--- read_and_plot.py
+++ read_and_plot.py
@@ -1,40 +1,41 @@
 # %% [ IMPORTS ]
 from specimen_files.unibar_redXs import fill_data, umax, bc_data, node_data
-from specimen_files.unibar_redXs import file_save_directory, probname
 from Plotting_file import *
 
 # %% pickle reading from save_file_location
 # for automated plotting get imports
-# =============================================================================
-# # manual plotting
-# fsd = r'H:\kp\Phase_field_code\results'
-# prob_name = 'unibar axial'
-# =============================================================================
+# manual plotting
 
-ref_loc = r'E:\Codes_GitSync\validation_data\unibar\redXS\actual_redXS.csv'
-file_path = fr'{file_save_directory}\{probname}\data.pkl'
+# fsd = r'H:/kp/Phase_field_code/results/single_edge_crck'
+fsd = r'H:/kp/Phase_field_code/results/unibar_tension'
+prob_name = '24-11-2025__lc=5'
+
+ref_loc = r'H:/kp/Phase_field_code/validationData/unibar_tension/2025_Pandey_adaptive_PFM.csv'
+# ref_loc = r'H:/kp/Phase_field_code/validationData/single_edge_crck/2025_edge_crck_plate_lc=0.03.csv'
+file_path = fr'{fsd}\{prob_name}\data.pkl'
 
 with open(file_path, 'rb') as f:
     loaded_variables = pickle.load(f)
     f.close()
 globals().update(loaded_variables)
 
-
 #%% PLOTTING
 stp = -1
 
 # mesh
-# plot_mesh(el_data, node_data)
+# plot_mesh(el_data, nd_data)
 
 # displacement contour
 plot_contour(plot_this=u1[f'{u1.columns[stp]}'], plot_on='nodes', crds=node_data, text_TT='U1 plot',
-             stpNum=int(u1.columns[stp][-1]), fill_data=fill_data)
+             stpNum=int(u1.columns[stp][5::]), fill_data=fill_data)
 
+plot_contour(plot_this=u2[f'{u2.columns[stp]}'], plot_on='nodes', crds=node_data, text_TT='U2 plot',
+             stpNum=int(u2.columns[stp][5::]), fill_data=fill_data)
 
 # damage contour
-
+plot_contour(plot_this=phi[f'{phi.columns[stp]}'], plot_on='nodes', crds=node_data, text_TT='Damage plot',
+             stpNum=int(phi.columns[stp][5::]), fill_data=fill_data)
 
 # Structural response / TSM
-# plot_structural_response(srmat)
-# plot_time_statistics(tsmat)
-
+plot_structural_response(srmat, validate=True, ref_loc=ref_loc)
+#plot_time_statistics(tsmat)
